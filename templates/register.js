470220fda354f3335bace6f101d7d9f4e635d03e
CarlosMerma08 <40005066+CarlosMerma@users.noreply.github.com>
2020-07-22 15:27:11 -0500

    x

--- templates/register.js
+++ templates/register.js
@@ -24,6 +24,7 @@ function registrar(){
             },
             error: function(response){
                 //alert(JSON.stringify(response));
+                alert("usuario no puede ser registrado")
             }
         });
 
